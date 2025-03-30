57e08fd2657d9944f739d7febf72260c0366b064
Tara Kaul <tarakaul@192.168.1.102>
2025-03-30 23:13:15 +0100

    Add cuisine filter

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
-import {processData} from './APIService'
+import {getCuisines, processData} from './APIService'
 import MainScreen from './MainScreen'
 import SavedRestaurantsScreen from './SavedRestaurantsScreen'
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Select from 'react-select'
 
 function App() {
   const [restaurants, setRestaurantData] = useState([]);
   const [savedRestaurants, setSavedRestaurants] = useState([]); 
   const [selectedMinRating, setSelectedMinRating] = useState(3);
+  const [cuisines, setCuisines] = useState([]); // list of all cuisines
+  const [selectedCuisines, setSelectedCuisines] = useState([]) // list of chosen cuisine in dropdown list
 
    // get restaurant data and set state
   useEffect(() => {
     const fetchData = async () => {
-      const data = await processData(selectedMinRating);
+      
+      const cuisines = await getCuisines();
+      setCuisines(cuisines)
+
+      const data = await processData(selectedMinRating, selectedCuisines);
       setRestaurantData(data);
     };
 
     fetchData();
-  }, [selectedMinRating]);
+  }, [selectedMinRating, selectedCuisines]);
+
+  const cuisineOptions = cuisines.map(cuisine => ({
+    value: cuisine,
+    label: cuisine
+  }));
+
+  // set the cuisines when selected
+  const handleCuisineSelect = (selected) => {
+    const selectedValues = selected.map(option => option.value);
+    setSelectedCuisines(selectedValues);
+
+  }
 
 
   return (
@@ -42,6 +61,15 @@ function App() {
                 <option value={4}>4 Stars</option>
                 <option value={5}>5 Stars</option>
               </select>
+
+              <label htmlFor="cuisineSelect">Select Cuisines: </label>
+
+              <Select 
+              id="cuisineSelect"
+                options={cuisineOptions} 
+                isMulti={true}
+                onChange={handleCuisineSelect}
+              />
 
               <Link to="/main">
                 <button>Start Swiping</button>
